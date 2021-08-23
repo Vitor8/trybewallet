34a04e90f6cdeb9dd90ab0b324cfe8a10273526b
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-22 22:32:46 -0300

    Updates

---
+++ precache-manifest.0d92bd2ced8cf0df1377f7931e25bb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840f5fc6f242cbe34140a0024bf1db46",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "a8436308a6a9fa20ee73",
+    "url": "/trybewallet/static/css/main.d0131999.chunk.css"
+  },
+  {
+    "revision": "79cd194f68216424c42b",
+    "url": "/trybewallet/static/js/2.32a36ade.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.32a36ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8436308a6a9fa20ee73",
+    "url": "/trybewallet/static/js/main.e703efc5.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
