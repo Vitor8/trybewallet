9fb576ebe0942eff2a9ff504cc5f41f7c4b64167
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-22 22:33:28 -0300

    Corrige rota github pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter>
+    <BrowserRouter basename="/trybewallet">
       <App />
     </BrowserRouter>
   </Provider>,
